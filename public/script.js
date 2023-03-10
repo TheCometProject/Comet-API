57e09455c8053ca43e9330186f9925b7a8f3b77b
1mt4y <t.madani@esi-sba.dz>
2023-03-10 14:10:46 +0100

    Deployment Testing

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 const socket = io()
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-  host: 'comet-api-testing.onrender.com',
+  host: 'peerjs-server-077w.onrender.com',
   port: 3001,
 })
 const myVideo = document.createElement('video')
