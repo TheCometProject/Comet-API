6bd99015f55b4a66a48da4daaa96d8267a2acb57
Lyna NOURA <l.noura@esi-sba.dz>
2023-03-28 21:39:49 +0100

    Added logout functionality

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const registerAPI = require('./register');
 const loginAPI = require("./login");
 const algerAPI = require("./alger");
-
+const logoutAPI = require("./logout");
 const sendConfirmationAPI = require("./send-confirmation-email");
 const confirmEmailAPI = require("./confirm-email");
 const refreshTokenAPI = require("./refresh-token");
@@ -15,7 +15,7 @@ const router = express.Router();
 router.use(registerAPI);
 router.use(loginAPI);
 router.use(algerAPI);
-
+router.use(logoutAPI);
 router.use(sendConfirmationAPI);
 router.use(confirmEmailAPI);
 router.use(refreshTokenAPI);
