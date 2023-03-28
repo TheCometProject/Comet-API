ab5ff6ecd385cd6ce4fbf51ebf1843997f2e0e8c
Lyna NOURA <l.noura@esi-sba.dz>
2023-03-28 23:05:10 +0100

    Added helper function and refactored code

--- src/api/send-confirmation-email.js
+++ src/api/send-confirmation-email.js
@@ -2,37 +2,36 @@ const express = require('express');
 const router = express.Router();
 const { v4: uuidv4 } = require('uuid');
 const User = require('../models/user');
-const SibApiV3Sdk = require('sib-api-v3-sdk');
-const defaultClient = SibApiV3Sdk.ApiClient.instance;
-const apiKey = defaultClient.authentications['api-key'];
-apiKey.apiKey = `${process.env.SENDINBLUE_API_KEY}`;
-const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+const { sendEmail } = require('../utils/email');
 
 router.post('/send-confirmation-email', async (req, res) => {
 
-    const { email } = req.body;
+    try {
 
-    // generate unique token
-    const confirmationToken = uuidv4();
+        const { email } = req.body;
 
-    // save confirmation token to user
-    await User.findOneAndUpdate({ email: email }, { confirmationToken: confirmationToken });
+        // generate unique token
+        const confirmationToken = uuidv4();
 
-    // send confirmation email
-    const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
-    sendSmtpEmail.subject = 'Confirm your email';
-    sendSmtpEmail.sender = { name: 'The Comet Project', email: 'thecometproject0@gmail.com' };
-    sendSmtpEmail.to = [{ email: email }];
-    sendSmtpEmail.textContent = 'Please click on the following link to confirm your email: http://localhost:5000/api/v1/confirm-email/' + confirmationToken;
+        // save confirmation token to user
+        await User.findOneAndUpdate({ email: email }, { confirmationToken: confirmationToken });
 
 
-    apiInstance.sendTransacEmail(sendSmtpEmail).then(() => {
-        console.log('Confirmation email sent');
+        const senderName = 'The Comet Project';
+        const senderEmail = 'thecometproject0@gmail.com';
+        const subject = 'Confirm your email';
+        const to = email;
+        const textContent = 'Please click on the following link to confirm your email: http://localhost:5000/api/v1/confirm-email/' + confirmationToken;
+
+        await sendEmail(senderName, senderEmail, to, subject, textContent);
+
         res.status(200).send('Confirmation email sent');
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).send('Error sending confirmation email');
-    });
+
+    } catch (error) {
+
+        res.status(500).send('Internal server error');
+
+    }
 });
 
 module.exports = router;
---
+++ src/utils/email.js
@@ -0,0 +1,28 @@
+const SibApiV3Sdk = require('sib-api-v3-sdk');
+const defaultClient = SibApiV3Sdk.ApiClient.instance;
+const apiKey = defaultClient.authentications['api-key'];
+apiKey.apiKey = `${process.env.SENDINBLUE_API_KEY}`;
+const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+
+async function sendEmail(senderName, senderEmail, to, subject, textContent) {
+
+      const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+      sendSmtpEmail.subject = subject;
+      sendSmtpEmail.sender = { name: senderName, email: senderEmail };
+      sendSmtpEmail.to = [{ email: to }];
+      sendSmtpEmail.textContent = textContent;
+
+      try {
+
+            await apiInstance.sendTransacEmail(sendSmtpEmail);
+            console.log('Email sent successfully');
+
+      } catch (err) {
+
+            console.log('Error sending email:', err);
+            throw err;
+
+      }
+}
+
+module.exports = { sendEmail };
