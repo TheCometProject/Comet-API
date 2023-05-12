cd3e318d365a299216068e14f94d29c68532a1a4
Lyna NOURA <l.noura@esi-sba.dz>
2023-05-12 18:42:06 +0100

    fixed contact us

--- src/api/contact-us.js
+++ src/api/contact-us.js
@@ -7,19 +7,19 @@ router.post('/contact-us', async (req, res, next) => {
 
     try {
 
-        const { name, email, subj, content } = req.body;
+        const { name, email, sub, content } = req.body;
 
         const senderName = name;
         const senderEmail = email;
         const to = 'thecometproject0@gmail.com';
-        const subject = subj;
+        const subject = sub;
         const textContent = content;
 
         await sendEmail(senderName, senderEmail, to, subject, textContent);
 
         return res
             .status(200)
-            .json({ message: "Contact us email sent successfully!", name, email, to, subj, content });
+            .json({ message: "Contact us email sent successfully!", name, email, to, sub, content });
 
 
     } catch (error) {
